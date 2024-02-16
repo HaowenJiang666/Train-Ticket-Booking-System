526b76f81d2294b97d9eb1d98352724a7f4caded
Haowen Jiang <hj323@cornell.edu>
2024-02-16 16:31:29 -0500

    前后端联调成功

--- member/src/main/java/com/hjiang/train/member/controller/MemberController.java
+++ member/src/main/java/com/hjiang/train/member/controller/MemberController.java
@@ -1,15 +1,14 @@
 package com.hjiang.train.member.controller;
 
 import com.hjiang.train.common.resp.CommonResp;
+import com.hjiang.train.member.req.MemberLoginReq;
 import com.hjiang.train.member.req.MemberRegisterReq;
 import com.hjiang.train.member.req.MemberSendCodeReq;
+import com.hjiang.train.member.resp.MemberLoginResp;
 import com.hjiang.train.member.service.MemberService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/member")
@@ -36,8 +35,14 @@ public class MemberController {
     }
 
     @PostMapping("/send-code")
-    public CommonResp<Long> sendCode(@Valid MemberSendCodeReq req) {
+    public CommonResp<Long> sendCode(@Valid @RequestBody MemberSendCodeReq req) {
         memberService.sendCode(req);
         return new CommonResp<>();
+    }
+
+    @PostMapping("/login")
+    public CommonResp<MemberLoginResp> login(@Valid MemberLoginReq req) {
+        MemberLoginResp resp = memberService.login(req);
+        return new CommonResp<>(resp);
     }
 }
--- member/src/main/java/com/hjiang/train/member/service/MemberService.java
+++ member/src/main/java/com/hjiang/train/member/service/MemberService.java
@@ -1,14 +1,18 @@
 package com.hjiang.train.member.service;
 
+import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.ObjectUtil;
 import com.hjiang.train.common.exception.BusinessException;
 import com.hjiang.train.common.exception.BusinessExceptionEnum;
 import com.hjiang.train.common.util.SnowUtil;
 import com.hjiang.train.member.domain.Member;
 import com.hjiang.train.member.domain.MemberExample;
 import com.hjiang.train.member.mapper.MemberMapper;
+import com.hjiang.train.member.req.MemberLoginReq;
 import com.hjiang.train.member.req.MemberRegisterReq;
 import com.hjiang.train.member.req.MemberSendCodeReq;
+import com.hjiang.train.member.resp.MemberLoginResp;
 import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,6 +27,7 @@ public class MemberService {
 
     @Resource
     private MemberMapper memberMapper;
+    private MemberLoginResp memberLoginResp;
 
     public int count() {
         return Math.toIntExact(memberMapper.countByExample(null));
@@ -31,10 +36,8 @@ public class MemberService {
     public long register(MemberRegisterReq req) {
         // 检查是否存在重复手机号
         String mobile = req.getMobile();
-        MemberExample memberExample = new MemberExample();
-        memberExample.createCriteria().andMobileEqualTo(mobile);
-        List<Member> list = memberMapper.selectByExample(memberExample);
-        if (CollUtil.isNotEmpty(list)) {
+        Member memberDB = selectByMobile(mobile);
+        if (ObjectUtil.isNotNull(memberDB)) {
             throw new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_EXIST);
         }
         Member member = new Member();
@@ -47,11 +50,8 @@ public class MemberService {
     public void sendCode(MemberSendCodeReq req) {
         // 检查是否存在重复手机号
         String mobile = req.getMobile();
-        MemberExample memberExample = new MemberExample();
-        memberExample.createCriteria().andMobileEqualTo(mobile);
-        List<Member> list = memberMapper.selectByExample(memberExample);
-        // 手机号不存在则插入记录
-        if (CollUtil.isEmpty(list)) {
+        Member memberDB = selectByMobile(mobile);
+        if (ObjectUtil.isNull(memberDB)) {
             LOG.info("手机号不存在, 插入一条记录");
             Member member = new Member();
             member.setId(SnowUtil.getSnowflakeNextId());
@@ -61,11 +61,35 @@ public class MemberService {
             LOG.info("手机号存在, 不插入记录");
         }
         // 生成验证码
-//        String code = RandomUtil.randomString(4);
         String code = "8888";
         LOG.info("生成短信验证码: {}", code);
         // 保存短信记录表: 手机号,验证码,有效期,是否已使用,业务类型,发送时间,使用时间
         LOG.info("生对接短信通道");
+    }
 
+
+    public MemberLoginResp login(MemberLoginReq req) {
+        String mobile = req.getMobile();
+        String code = req.getCode();
+        Member memberDB = selectByMobile(mobile);
+        if (ObjectUtil.isNull(memberDB)) {
+            throw new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_NOT_EXIST);
+        }
+        // 校验短信验证码
+        if (!"8888".equals(code)) {
+            throw new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_CODE_ERROR);
+        }
+        return BeanUtil.copyProperties(memberDB, MemberLoginResp.class);
+    }
+
+    private Member selectByMobile(String mobile) {
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> list = memberMapper.selectByExample(memberExample);
+        if (CollUtil.isEmpty(list)) {
+            return null;
+        } else {
+            return list.get(0);
+        }
     }
 }
