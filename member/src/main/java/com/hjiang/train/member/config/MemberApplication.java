a92bfe459355d524e1e32e44f362a99402d920db
Haowen Jiang <hj323@cornell.edu>
2024-02-16 00:57:43 -0500

    Mybatis integration, successfully connect DB

--- member/src/main/java/com/hjiang/train/member/config/MemberApplication.java
+++ member/src/main/java/com/hjiang/train/member/config/MemberApplication.java
@@ -1,5 +1,6 @@
 package com.hjiang.train.member.config;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -9,6 +10,7 @@ import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.hjiang")
+@MapperScan("com.hjiang.train.member.mapper")
 public class MemberApplication {
 	private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
 
---
+++ member/src/main/java/com/hjiang/train/member/mapper/MemberMapper.java
@@ -0,0 +1,6 @@
+package com.hjiang.train.member.mapper;
+
+public interface MemberMapper {
+
+    int count();
+}
