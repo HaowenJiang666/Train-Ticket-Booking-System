2aa4e90b3978f9a1e106ddbad24a418873e8a8c0
Haowen Jiang <hj323@cornell.edu>
2024-02-17 15:48:20 -0500

    axios登录拦截

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -1,6 +1,12 @@
 <template>
     <a-layout-header class="header">
       <div class="logo" />
+      <div style="float: right; color: white;">
+       您好：{{member.mobile}} &nbsp;&nbsp;
+       <router-link to="/login" style="color: white;">
+         退出登录
+       </router-link>
+     </div>
       <a-menu
           v-model:selectedKeys="selectedKeys1"
           theme="dark"
@@ -15,14 +21,17 @@
   </template>
   
   <script>
-  import {defineComponent, ref} from 'vue';
+  import store from '@/store';
+import {defineComponent, ref} from 'vue';
   
   export default defineComponent({
     name: "the-header-view",
     setup() {
+        let member = store.state.member;
   
       return {
         selectedKeys1: ref(['2']),
+        member
       };
     },
   });
