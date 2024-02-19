7d4ba4c243c7496313d001223f1e5f6f12df5aff
Haowen Jiang <hj323@cornell.edu>
2024-02-19 15:43:40 -0500

    增加火车车厢表,生成持久层与前后端代码

---
+++ business/src/main/java/com/hjiang/train/business/req/TrainCarriageQueryReq.java
@@ -0,0 +1,12 @@
+package com.hjiang.train.business.req;
+
+import com.hjiang.train.common.req.PageReq;
+
+public class TrainCarriageQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "TrainCarriageQueryReq{" +
+                "} " + super.toString();
+    }
+}
