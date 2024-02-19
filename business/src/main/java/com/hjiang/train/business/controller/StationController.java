64c3d066e534b570a134391505eeed66bf06f24f
Haowen Jiang <hj323@cornell.edu>
2024-02-19 14:52:58 -0500

    生成持久层与前后端代码

---
+++ business/src/main/java/com/hjiang/train/business/controller/StationController.java
@@ -0,0 +1,40 @@
+package com.hjiang.train.business.controller;
+
+import com.hjiang.train.common.context.LoginMemberContext;
+import com.hjiang.train.common.resp.CommonResp;
+import com.hjiang.train.common.resp.PageResp;
+import com.hjiang.train.business.req.StationQueryReq;
+import com.hjiang.train.business.req.StationSaveReq;
+import com.hjiang.train.business.resp.StationQueryResp;
+import com.hjiang.train.business.service.StationService;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/station")
+public class StationController {
+
+    @Resource
+    private StationService stationService;
+
+    @PostMapping("/save")
+    public CommonResp<Object> save(@Valid @RequestBody StationSaveReq req) {
+        stationService.save(req);
+        return new CommonResp<>();
+    }
+
+    @GetMapping("/query-list")
+    public CommonResp<PageResp<StationQueryResp>> queryList(@Valid StationQueryReq req) {
+        PageResp<StationQueryResp> list = stationService.queryList(req);
+        return new CommonResp<>(list);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp<Object> delete(@PathVariable Long id) {
+        stationService.delete(id);
+        return new CommonResp<>();
+    }
+
+
+}
---
+++ business/src/main/java/com/hjiang/train/business/req/StationQueryReq.java
@@ -0,0 +1,12 @@
+package com.hjiang.train.business.req;
+
+import com.hjiang.train.common.req.PageReq;
+
+public class StationQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "StationQueryReq{" +
+                "} " + super.toString();
+    }
+}
